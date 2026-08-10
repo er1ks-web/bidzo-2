06f43b9c7790e9cc8c193f91eb810c458f7b99a5
er1ks-web <rxstrazdins@gmail.com>
2026-08-10 12:17:05 +0300

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Toaster } from 'sonner'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -110,6 +111,7 @@ function App() {
               }}
             />
             <SpeedInsights />
+            <Analytics />
           </I18nProvider>
         </QueryClientProvider>
       </AuthProvider>
